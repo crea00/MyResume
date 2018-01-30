1c235213e35e3944b579f3b2771d4c9a3c1c9fee
yongbum <zlzlzl002@naver.com>
2018-01-30 17:30:31 +0900

    cc

--- src/main/java/com/project/myresume/HomeController.java
+++ src/main/java/com/project/myresume/HomeController.java
@@ -1,8 +1,12 @@
 package com.project.myresume;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,27 +15,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	@RequestMapping("/home")
+	public String home(HttpServletRequest request) {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		//가상의 공지 사항 
+		List<String> news=new ArrayList<>();
+		news.add("하나");
+		news.add("두울");
+		news.add("세엣");
+		news.add("어쩌구.. 저쩌구...");
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+		request.setAttribute("news", news);
 		
 		return "home";
 	}
