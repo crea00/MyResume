2f45101d8e671a69f540da235eae33e710013f55
leeminkyoung <4187503@gmail.com>
2018-02-06 13:28:34 +0900

    a

--- src/main/java/com/project/myresume/users/controller/UsersController.java
+++ src/main/java/com/project/myresume/users/controller/UsersController.java
@@ -22,10 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 import com.project.myresume.profile.service.AcService;
 import com.project.myresume.profile.service.EduService;
 import com.project.myresume.profile.service.ExpsService;
